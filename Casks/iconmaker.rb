47192c81b692f83549d86e2c685ccbdf945590c1
郝志宇 <haozhiyu@ttmv.com>
2023-03-24 12:10:55 +0800

    ...

--- Casks/iconmaker.rb
+++ Casks/iconmaker.rb
@@ -2,7 +2,7 @@ cask "iconmaker" do
   version "1.0.0"
   sha256 "046e7cdf5e83b45dea425c1d5c85dd8f125b6e66d2573ef531d5401391a43918"
 
-  url "https://github.com/haozhiyu1990/iconMaker/archive/#{version}.tar.gz"
+  url "https://github.com/haozhiyu1990/iconMaker/releases/download/#{version}/App.Icon.Maker.zip"
   name "iconMaker"
   desc "App Icon Maker"
   homepage "https://github.com/haozhiyu1990/iconMaker"
