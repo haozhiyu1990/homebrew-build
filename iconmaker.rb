e8affbd9fcf470338287e076ee58f39285f8a7b0
郝志宇 <haozhiyu@ttmv.com>
2023-03-24 11:29:29 +0800

    ...

---
+++ iconmaker.rb
@@ -0,0 +1,11 @@
+cask "iconmaker" do
+  version "1.0.0"
+  sha256 "046e7cdf5e83b45dea425c1d5c85dd8f125b6e66d2573ef531d5401391a43918"
+
+  url "https://github.com/haozhiyu1990/iconMaker/archive/#{version}.tar.gz"
+  name "iconMaker"
+  desc "App Icon Maker"
+  homepage "https://github.com/haozhiyu1990/iconMaker"
+
+  app "App Icon Maker.app"
+end
